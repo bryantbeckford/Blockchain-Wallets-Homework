62c8267d116a3a6d6c4561baf36507619c831e96
Bryant Beckford <bryantbeckford@outlook.com>
2023-11-13 15:07:36 -0500

    updating repo

--- Instructions/Starter-Code/crypto_wallet.py
+++ Instructions/Starter-Code/crypto_wallet.py
@@ -25,7 +25,7 @@ def generate_account():
     """Create a digital wallet and Ethereum account from a mnemonic seed phrase."""
     # Fetch mnemonic from environment variable.
     mnemonic = os.getenv("MNEMONIC")
-
+    print(f"Mnemonic: {mnemonic}, Type: {type(mnemonic)}")
     # Create Wallet Object
     wallet = Wallet(mnemonic)
 
